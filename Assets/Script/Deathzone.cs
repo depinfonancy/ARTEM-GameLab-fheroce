449d599bcdcf8bb4c7a051a913e2b9f917fe1219
NthnMgns <nathan.miguens@gmail.com>
2019-04-11 18:26:01 +0200

    1er commit
    Ajout :
    - map créée sur C4D
    - player (sphère) qui se déplace
    - caméra suivant le player
    - deathzone
    - Checkpoint
    - Un pendule
    - De la lumière

---
+++ Assets/Script/Deathzone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathzone : MonoBehaviour
+{
+
+    public Vector3 respawnPositions;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Player")
+        {
+            other.transform.position = respawnPositions;
+        }
+    }
+}
