bc0b08c3139ff9093836b612994fa4fd5a0c03ee
NthnMgns <nathan.miguens@gmail.com>
2019-05-28 11:57:27 +0200

    Version 2805
    Correction bug
    Début piege
    Création des prefab

--- Assets/Blocs/BlocsGenerator.cs
+++ Assets/Blocs/BlocsGenerator.cs
@@ -5,44 +5,44 @@ using UnityEngine;
 public class BlocsGenerator : MonoBehaviour
 {
     public int lengthBloc = 4;
+
     public GameObject player;
     public GameObject spot;
     public GameObject eclairage;
 
-    private GameObject[] tableauDesCopiesDeBlocs = new GameObject[30];
-    private GameObject[] monstres;
+    private int longueurTableau = 36;
+    private GameObject[] tableauDesCopiesDeBlocs = new GameObject[36];
+    public GameObject[] monstres = new GameObject[6];
   
     private int parcoursTableau = 0;
   
-    private GameObject[] tableauDeBlocs;
+    public GameObject[] tableauDeBlocs = new GameObject[8];
     
-    private int NumberOfBlocs = 1;
     private int blocNumberAdded;
    
     private float zLastBlocCenter = 0.0f;
 
     void Start()
     {
-        tableauDeBlocs = GameObject.FindGameObjectsWithTag("Blocs");
-        monstres = GameObject.FindGameObjectsWithTag("Enemy");
+        
+        
      }
 
 // Update is called once per frame
 void Update()
-    { if (player.transform.position.z >= (NumberOfBlocs - 10) * lengthBloc)
+    { if (player.transform.position.z >= (parcoursTableau - 26) * lengthBloc)
         {
             blocNumberAdded = Random.Range(0, tableauDeBlocs.Length - 1);
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 9; i++)
             {
-                zLastBlocCenter += lengthBloc;
-                NumberOfBlocs++;
-                Destroy(tableauDesCopiesDeBlocs[parcoursTableau % 30]);
-                tableauDesCopiesDeBlocs[parcoursTableau % 30] = Instantiate(tableauDeBlocs[blocNumberAdded], new Vector3(0.0f, 0.0f, zLastBlocCenter), Quaternion.identity);
-                parcoursTableau++;
+                
+                Destroy(tableauDesCopiesDeBlocs[parcoursTableau % longueurTableau]);
+                tableauDesCopiesDeBlocs[parcoursTableau % longueurTableau] = Instantiate(tableauDeBlocs[blocNumberAdded], new Vector3(0.0f, 0.0f, zLastBlocCenter), Quaternion.identity);
+                parcoursTableau++; zLastBlocCenter += lengthBloc;
                 if (i == 5)
                 {
-                    Destroy(tableauDesCopiesDeBlocs[parcoursTableau % 30]);
-                    tableauDesCopiesDeBlocs[parcoursTableau % 30] = Instantiate(eclairage, new Vector3(0.0f, 0.0f, zLastBlocCenter), Quaternion.identity);
+                    Destroy(tableauDesCopiesDeBlocs[parcoursTableau % longueurTableau]);
+                    tableauDesCopiesDeBlocs[parcoursTableau % longueurTableau] = Instantiate(eclairage, new Vector3(0.0f, 0.0f, zLastBlocCenter), Quaternion.identity);
                     parcoursTableau++;
                 }
                 int monstreNumberAdded = Random.Range(0, monstres.Length - 1);
@@ -51,8 +51,8 @@ void Update()
                 
                 
             }
-            Destroy(tableauDesCopiesDeBlocs[parcoursTableau % 30]);
-            tableauDesCopiesDeBlocs[parcoursTableau % 30] = Instantiate(spot, new Vector3(0.0f, 0.0f, zLastBlocCenter), Quaternion.identity);
+            Destroy(tableauDesCopiesDeBlocs[parcoursTableau % longueurTableau]);
+            tableauDesCopiesDeBlocs[parcoursTableau % longueurTableau] = Instantiate(spot, new Vector3(0.0f, 0.0f, zLastBlocCenter), Quaternion.identity);
             parcoursTableau++;
 
             
